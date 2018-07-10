b0e180eb77a8d3c135855cd8118ff8c0f533f991
Kurtis Trainor <kurtistrainor@gmail.com>
2018-07-10 11:11:02 -0400

    added more unit tests for other fields in model

--- api/models.py
+++ api/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class DeviceList(models.Model):
     """This class is the device list model."""
     device_name = models.CharField(max_length=255, blank=False)
     timestamp = models.DateTimeField(auto_now_add=True)
+    battery_status = models.IntegerField(default=0, validators=[MaxValueValidator(100), MinValueValidator(0)])
+    longitude = models.DecimalField(default=0.000000, max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(default=0.000000, max_digits=9, decimal_places=6)
 
     def __str__(self):
         """Nice readablility."""
--- api/tests.py
+++ api/tests.py
@@ -7,10 +7,10 @@ class ModelTestCase(TestCase):
     def setup_device(self):
         """Tryna make a phone with a bunch of stuff, yep..."""
         self.device_name = "The Bond Phone"
-        #self.battery_status = "100"
-        #self.longitude = "51.509865"
-        #self.latitude = "-0.118092"
-        self.devicelist = DeviceList(device_name=self.device_name)
+        self.battery_status = "100"
+        self.longitude = "51.509865"
+        self.latitude = "-0.118092"
+        self.devicelist = DeviceList(device_name=self.device_name, battery_status=self.battery_status, longitude=self.longitude, latitude=self.latitude)
 
     def save_device(self):
         """Makin sure we can save phones to the database."""
